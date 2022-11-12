da98e623fe4d63b4b7c552d85f55e4b765c11ac6
JazzyMcJazz <larsriel@gmail.com>
2022-11-12 22:02:58 +0100

    Update build-tools.sh

--- build-tools.sh
+++ build-tools.sh
@@ -1,31 +1,48 @@
 #!/bin/bash
 
-services=('client')
+action=$1; shift
+services=($@)
 
-action=$1
+echo ""
+
+if [ "$action" == "build" ];
+then
+  if (( ${#services[@]} == 0 ))
+  then
+    action=""
+  fi
+  for service in ${services[@]}
+    do
+      if ! [[ -f Dockerfiles/$service.Dockerfile ]]
+      then
+        echo -e "Error! Dockerfiles/$service.Dockerfile doesn't exist"
+        exit 1
+      fi
+    done
+fi
 
 case ${action} in
-    build)
-    echo "Building projects..."
+  build)
+    echo "Building services..."
     for service in ${services[@]}
-        do
-            echo "building image for $service"
-            docker build -t teamcity-dd-$service -f Dockerfiles/$service.Dockerfile .
-        done
-    ;;
-    run)
+      do
+        echo "Building image for $service"
+        docker build -t teamcity-dd-$service -f Dockerfiles/$service.Dockerfile .
+      done
+  ;;
+  run)
     echo "Running Docker Compose..."
     docker compose up -d
-    ;;
-    *)
-    echo -e "Usage:"
-    echo " ./build-tools.sh build  - build docker images"
-    echo " ./build-tools.sh run    - deploy images with docker compose"
+  ;;
+  --help)
+    echo " ./build-tools.sh build ...args  - build docker images"
+    echo " ./build-tools.sh run            - deploy images with docker compose"
     echo ""
-    echo "Remember edit the services array at the top of this script to include all the services that you want to build"
-    echo "The name of the service must correspond to a Dockerfile in the Dockerfiles folder"
-    echo "Example: myservice.Dockerfile"
-    echo "It must also correspond to the image name in docker-compose.yml"
-    echo "Example: image: teamcity-dd-myservice"
+    echo " ...args must correspond to 'arg.Dockerfile' in Dockerfiles/ and 'image: teamcity-dd-arg' in 'docker-compose.yml'"
+    exit 1
+  ;;
+  *)
+    echo -e " Invalid command"
+    echo " See './build-tools.sh --help'"
     exit 1
 esac
