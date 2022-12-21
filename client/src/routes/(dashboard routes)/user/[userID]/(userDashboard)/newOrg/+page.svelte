178a4cabb9fd8f900770ed29564d846d4678feac
JazzyMcJazz <larsriel@gmail.com>
2022-12-21 16:53:58 +0100

    Update +page.svelte

--- client/src/routes/(dashboard routes)/user/[userID]/(userDashboard)/newOrg/+page.svelte
+++ client/src/routes/(dashboard routes)/user/[userID]/(userDashboard)/newOrg/+page.svelte
@@ -1,28 +1,27 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    
-    export let data: PageData;
-    let userEmail = data.user.email
+    import { page } from '$app/stores';
+
+    const { user } = $page.data;
+    let userEmail = user.email
 </script>
 
 <div class="form-wrapper">
-    <form class="form-form" method="POST" action="?/newOrg"> 
+    <form class="form-form" method="POST" action="?/newOrg">
             <h1> Create New Organization</h1>
-            <input class="input-name" name="name" type="text" placeholder="Organization Name">   
-            <input class="input-name" name="contact_phone" type="text" placeholder="Contact tlf nr."> 
-            <input class="input-email" name="contact_email" type="text"  placeholder="Email tlf nr.">
+            <input class="input-name" name="name" type="text" placeholder="Organization Name">
+            <input class="input-name" name="contact_phone" type="text" placeholder="Contact Phone">
+            <input class="input-email" name="contact_email" type="text"  placeholder="Contact Email">
             <div class="checkbox-div">
                 <input class="checkbox" type="checkbox" name="terms">
                 <label for="terms"> I agree to terms and conditions</label>
             </div>
-             
+
             <div class="inner-btn-div">
             <button class="btn" type="reset">Reset</button>
             <button class="btn" type="submit">Create</button>
         </div>
 
-    </form>    
+    </form>
 </div>
 
 <style>
@@ -43,26 +42,26 @@
     font-size: 15px;
     line-height: 20px;
     padding: 10px;
-     
+
     font-family: 'Oswald';
-    font-weight: normal;  
-    font-style: normal; 
-    font-variant: normal; 
-    text-transform: none;   
-    
+    font-weight: normal;
+    font-style: normal;
+    font-variant: normal;
+    text-transform: none;
+
     display: inline-block;
-    color: rgb(255, 255, 255); 
-    
+    color: rgb(255, 255, 255);
+
     }
 
     input {
     background:#1e1844ab;
     margin: 5px;
     width: auto;
-    border: 2px solid #798AC5; 
+    border: 2px solid #798AC5;
     }
     input:focus{
-        background: #798AC5; 
+        background: #798AC5;
         outline: 5px solid #1e1844ab;
         color: #ffffff
     }
@@ -71,21 +70,21 @@
     background: 0;
     margin: 5px;
     width: 50%;
-    border: 3px solid #798AC5; 
-    
+    border: 3px solid #798AC5;
+
     }
 
     .btn:hover {
-        background: #798AC5; 
+        background: #798AC5;
     }
 
     .btn:active {
-        background: #144E75; 
-    } 
+        background: #144E75;
+    }
     h1 {
         text-align: center;
         margin-top: 0;
-        
+
     }
     .checkbox-div {
         display: flex;
