40a8d0cd5335b37966b5d7a80dfafd859a984adc
Alex Hersfeldt <alexhersfeldt@gmail.com>
2022-12-31 23:10:36 +0100

    updated button

--- client/src/lib/Components/Button.svelte
+++ client/src/lib/Components/Button.svelte
@@ -1,24 +1,88 @@
-
 <script>
-
-    /**
-	 * @type {any}
-	 */
-     export let btnClick;
-    /**
-	 * @type {any}
-	 */
-     export let btnTitle = '';
-     export let width = "auto"
+  /**
+ * @type {any}
+ */
+    export let btnClick = null;
+    export let btnTitle = '';
+    export let width = "auto"
     export let active = false;
+    export let colorLight = '#798AC5'
+    export let colorMedium = '#1e1844ab'
+    export let colorDark = '#43448d'
+    export let btnType = "submit"
 </script>
 
-<button class="btn {active ? 'active' : ''}" style="width:{width};" on:click={btnClick} >{btnTitle}<slot/></button>
+<button type="{btnType}" class="{active ? 'active' : 'button'}"  id="button-3" style="width:{width};--color1:{colorLight};--color2:{colorMedium};--color3:{colorDark};" on:click={btnClick}><slot/>
+<div id="circle" style ="--color1:{colorLight};"></div>
+<p class="text"> {btnTitle} </p>
+</button>
 
 <style>
 
+.button .text {
+  position: relative;
+  transition: all .45s ease-Out;
+}
 
-    .active {
-        background: #43448d;
-    }
+#circle {
+  width: 0%;
+  height: 0%;
+  opacity: 0;
+  line-height: 40px;
+  border-radius: 50%;
+  background: var(--color1);
+  position: absolute;
+  transition: all .5s ease-Out;
+}
+
+.button:hover #circle {
+  width: 150%;
+  height: 150%;
+  opacity: 0.8;
+}
+
+.button, .active{
+    
+  display: inline-flex;
+  height: 40px;
+  width: 150px;
+  border: 3px solid var(--color1);
+  margin: 20px 20px 20px 20px;
+  color: #BFC0C0;
+  text-decoration: none;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  background: 0;
+  border-radius: 15px;
+  cursor: pointer;
+}
+.button {
+  position: relative;
+  overflow: hidden;
+  
+}
+.active {
+  background: var(--color2);
+  position: relative;
+  overflow: hidden;
+}
+.text {
+  color: rgb(255, 255, 255);
+  font-size: 20px;
+  line-height: 20px;
+  padding: 10px;
+  font-family: 'Oswald';
+  font-weight: normal;
+  font-style: normal;
+  font-variant: normal;
+  
+}
+.button:hover {
+  border: 3px solid var(--color3);
+  box-shadow:0px 0px 0px 2px var(--color3) inset;
+}
+.button:active {
+  background: var(--color2);
+}
 </style>
