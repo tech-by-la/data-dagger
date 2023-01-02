3a79f571591c98ae529f78a4cf29fd8051b20dcb
Alex Hersfeldt <alexhersfeldt@gmail.com>
2023-01-02 10:56:39 +0100

    Last button update i swear!

--- client/src/lib/Components/Button.svelte
+++ client/src/lib/Components/Button.svelte
@@ -12,13 +12,15 @@
     export let btnType = "submit"
 </script>
 
-<button type="{btnType}" class="{active ? 'active' : 'button'}"  id="button-3" style="width:{width};--color1:{colorLight};--color2:{colorMedium};--color3:{colorDark};" on:click={btnClick}><slot/>
-<div id="circle" style ="--color1:{colorLight};"></div>
-<p class="text"> {btnTitle} </p>
-</button>
+  <button type="{btnType}" class="{active ? 'active' : 'button'}" style="width:{width};--color1:{colorLight};--color2:{colorMedium};--color3:{colorDark};" on:click={btnClick}><slot/>
+    <div id="circle" style ="--color1:{colorLight};"></div>
+    <p class="text"> {btnTitle} </p>
+    </button>
+
+
 
 <style>
-
+    
     .button .text {
       position: relative;
       transition: all .45s ease-Out;
@@ -28,7 +30,7 @@
       width: 0%;
       height: 0%;
       opacity: 0;
-      line-height: 40px;
+      line-height: 20px;
       border-radius: 50%;
       background: var(--color1);
       position: absolute;
@@ -38,7 +40,8 @@
     .button:hover #circle {
       width: 150%;
       height: 150%;
-      opacity: 0.7;
+      opacity: 0.5;
+      
     }
 
     .button, .active{
@@ -80,13 +83,15 @@
 
     }
     .button:hover {
-      /* border: 3px solid var(--color3) inset; */
+      /* border: 1px solid var(--color3) inset, 2px solid var(--color3); */
       border: 2px solid var(--color2);
-      /*padding: 2px;*/
+      /* outline: inset 2px solid var(--color2) ; */
+      /* padding: 2px; */
       /* box-shadow:0px 0px 0px 2px var(--color3) ; */
-      box-shadow: 0px 0px 10px 5px var(--color2) ;
-      transition: all 0.4s ease-in-out;
-      animation: pulse 1s linear infinite;
+      box-shadow: 0px 0px 10px 10px var(--color3), 0px 0px 10px 5px var(--color1) inset, 0px 0px 2px 2px var(--color1)  ;
+      transition: all 0.5s ease;
+      /* animation: pulse 2s ease infinite; */
+      /* outline: 5px solid var(--color2) inset; */
     }
     .button:active {
       background: var(--color2);
@@ -94,40 +99,40 @@
     @keyframes pulse {
         0% {
             transform: scale(1);
-            box-shadow: 1px 0 10px 5px var(--color2);
+            box-shadow: 1px 0 10px 5px var(--color2), 0px 0px 5px 2px var(--color2);
         }
         12.5% {
             transform: scale(1);
-            box-shadow: 0.5px 0.5px 10px 5px var(--color2);
+            box-shadow: 0.5px 0.5px 10px 5px var(--color2), 0px 0px 5px 2px var(--color2);
         }
         25% {
             transform: scale(1);
-            box-shadow: 0 1px 10px 5px var(--color2);
+            box-shadow: 0 1px 10px 5px var(--color2), 0px 0px 5px 2px var(--color2);
         }
         37.5% {
             transform: scale(1);
-            box-shadow: -0.5px 0.5px 10px 5px var(--color2);
+            box-shadow: -0.5px 0.5px 10px 5px var(--color2), 0px 0px 5px 2px var(--color2);
         }
         50% {
             transform: scale(1);
-            box-shadow: -1px 0 10px 5px var(--color2);
+            box-shadow: -1px 0 10px 5px var(--color2), 0px 0px 5px 2px var(--color2);
         }
         62.5% {
             transform: scale(1);
-            box-shadow: -0.5px -0.5px 10px 5px var(--color2);
+            box-shadow: -0.5px -0.5px 10px 5px var(--color2), 0px 0px 5px 2px var(--color2);
         }
 
         75% {
             transform: scale(1);
-            box-shadow: 0 -1px 10px 5px var(--color2);
+            box-shadow: 0 -1px 10px 5px var(--color2), 0px 0px 5px 2px var(--color2);
         }
         88.5% {
             transform: scale(1);
-            box-shadow: 0.5px -0.5px 10px 5px var(--color2);
+            box-shadow: 0.5px -0.5px 10px 5px var(--color2), 0px 0px 5px 2px var(--color2);
         }
         100% {
             transform: scale(1);
-            box-shadow: 1px 0px 10px 5px var(--color2);
+            box-shadow: 1px 0px 10px 5px var(--color2), 0px 0px 5px 2px var(--color2);
         }
     }
 </style>
