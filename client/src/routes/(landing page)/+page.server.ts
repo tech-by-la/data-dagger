0dfaafb883780e0b5df4433a37d4b504b3af3e5f
JazzyMcJazz <larsriel@gmail.com>
2023-01-15 21:18:44 +0100

    exam

--- client/src/routes/(landing page)/+page.server.ts
+++ client/src/routes/(landing page)/+page.server.ts
@@ -4,7 +4,7 @@ const text = {
 	introText: "Welcome to Data Dagger",
 	aboutText1:"Data Dagger is a platform to streamline production of and/or Quality assurance of certain datasets.",
 	aboutText2:"We have created an efficient workflow to manage and control how your data is managed and who has access to it",
-	aboutText3:"For now we support production of Machine learning models for classification of raster geodata. But in the future we will provide support for quality assurance of vector geodata and other forms of machine learning models (images/files)",
+	aboutText3:"For now we support quality assurance tasks of geodata.",
 	howToText1:"How It works",
 	howToText2:"Start by registering a User. Once you’ve done that you will have the option to create an Organization, of which you will be the administrator.",
 	howToText3:"This Organization can then start different projects. You can invite others to join your Organization and they will be able to work on your different projects - Only the admins can start new projects",
